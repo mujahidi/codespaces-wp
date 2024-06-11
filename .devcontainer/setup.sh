2a9b10726daa1fe614cc57d649bdb86e170174df
Mujahid Ishtiaq <mujahid158@gmail.com>
2024-06-12 00:48:12 +0500

    Updated setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -18,30 +18,13 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>setup.log 2>&1
 
-# Prepare a nice name from project name for the site title.
-function getTitleFromSlug() {
-	local _slug=${SLUG//-/ }
-	local __slug=${_slug//_/ }
-	local ___slug=($__slug)
-	echo "${___slug[@]^}"
-}
-
 source ~/.bashrc
 
-# Install dependencies
-cd /var/www/html/wp-content/${PROJECT_TYPE}s/${SLUG}/
-npm i && npm run build
-
-if [[ $PROJECT_TYPE == "plugin" ]]; then
-# Install WordPress and activate the plugin
-	cd /var/www/html/
-	echo "Setting up WordPress at $SITE_HOST"
-	wp db reset --yes
-	wp core install --url="$SITE_HOST" --title="$(getTitleFromSlug) Development" --admin_user="admin" --admin_email="admin@example.com" --admin_password="password" --skip-email
-
-	echo "Activate $SLUG"
-	wp $PROJECT_TYPE activate $SLUG $WPARG
-fi
+# Install WordPress
+cd /var/www/html/
+echo "Setting up WordPress at $SITE_HOST"
+wp db reset --yes
+wp core install --url="$SITE_HOST" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_email="$ADMIN_EMAIL" --admin_password="$ADMIN_PASS" --skip-email
 
 sudo chown www-data:www-data /var/www/html/xdebug.log
 sudo chmod 664 /var/www/html/xdebug.log
